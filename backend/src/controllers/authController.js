0787d73e3c95a733887f594a5dcac592cb844838
Manuel <m.p.porroa@maxedv-beratung.de>
2024-06-25 15:40:40 +0200

    post user to mongodb

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,6 +1,26 @@
-export const register = (req, res) => {
-  res.send('register');
+import e from 'express';
+import User from '../models/userModel.js';
+
+//Register
+export const register = async (req, res) => {
+  const { email, password, username } = req.body;
+
+  try {
+    //we create an object to create a new user
+    const newUser = new User({
+      username,
+      email,
+      password,
+    });
+    console.log(newUser);
+    await newUser.save(); //to save in the db
+    res.send('register');
+  } catch (error) {
+    console.log(error);
+  }
 };
+
+//Login
 export const login = (req, res) => {
   res.send('login');
 };
