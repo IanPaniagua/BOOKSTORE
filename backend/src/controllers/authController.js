f5cd70550fbfeb1949a471fb8b96f12a2ea8091e
Manuel <m.p.porroa@maxedv-beratung.de>
2024-06-26 10:06:00 +0200

    addint bcryptjs and token to the register

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,20 +1,47 @@
-import e from 'express';
+import express from 'express';
 import User from '../models/userModel.js';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
 //Register
 export const register = async (req, res) => {
   const { email, password, username } = req.body;
 
   try {
+    const passwordHash = await bcrypt.hash(password, 10);
+
     //we create an object to create a new user
     const newUser = new User({
       username,
       email,
-      password,
+      password: passwordHash,
     });
     console.log(newUser);
-    const userSaved = await newUser.save(); //to save in the db
-    res.send(userSaved);
+    const userSaved = await newUser.save(); //to save in the db (all the data)
+
+    jwt.sign(
+      {
+        id: userSaved._id,
+      },
+      'secret123',
+      {
+        expiresIn: '1d',
+      },
+      (err, token) => {
+        if (err) consonle.log(err);
+        res.cookie('token', token);
+        res.json({
+          message: 'User created successfully',
+        });
+      },
+    );
+
+    // res.send(userSaved);    (we need create all but no return all the info)
+    // res.json({
+    //   id: userSaved._id,
+    //   username: userSaved.email,
+    //   email: userSaved.email,
+    // }); //(only necesary data for the frontend)
   } catch (error) {
     console.log(error);
   }
